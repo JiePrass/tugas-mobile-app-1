0a81fdb4e32c049da6c3a8fdfff788a19afba212
renjieprass <renjieprass@gmail.com>
2024-08-28 06:17:43 +0000

    done....

---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    Center(child: Text('Home Screen')),
+    Center(child: Text('Schedule Screen')),
+    Center(child: Text('Information Screen')),
+    Center(child: Text('Card Screen')),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'SMKN4 Mobile Apps',
+          style: TextStyle(
+              color: Color.fromRGBO(255, 255, 255, 1),
+              fontWeight: FontWeight.w600),
+        ),
+        backgroundColor: Color.fromARGB(255, 60, 80, 255),
+      ),
+      body: _widgetOptions.elementAt(_selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.schedule),
+            label: 'Schedule',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.info_outline_rounded),
+            label: 'Information',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.card_membership_outlined),
+            label: 'Card',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blueAccent,
+        unselectedItemColor: Colors.grey,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
